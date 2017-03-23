aa6017c768fa784403a92c348284f36d238b5f69
Vipin Kumar V <vipink1203@users.noreply.github.com>
2017-03-23 14:41:28 +0530

    permission line added

--- delopy_rsync.sh
+++ delopy_rsync.sh
@@ -6,4 +6,4 @@
 #	Last	Edited:	March	23	2017
 
 
-rsync -rzcSLhe "ssh -p2200" Public/ root@host.example.com:/home/vipinkumar/public_html
+rsync -rzcSLhe "ssh -p2200" Public/ root@host.example.com:/home/vipinkumar/public_html && ssh -p2200 root@host.example.com "chown -R vipinkumar:vipinkumar /home/vipinkumar/public_html/"
