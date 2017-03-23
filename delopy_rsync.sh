9991af02b0c1d6b5507631816baabdd2780ab9d9
Vipin Kumar V <vipink1203@users.noreply.github.com>
2017-03-23 14:38:12 +0530

    deploy using rsync script added

---
+++ delopy_rsync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#	Author:	@vipink1203
+#	Web:	www.webnuxpro.com
+#	Automated deployment Script using rsync (secure copy - ssh) 	
+#	Last	Edited:	March	23	2017
+
+
+rsync -rzcSLhe "ssh -p2200" Public/ root@host.example.com:/home/vipinkumar/public_html
