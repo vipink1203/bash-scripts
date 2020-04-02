1503de7a160f8b110ed0c84232c0d4c9155a1767
Vipin Kumar <vipink1203@gmail.com>
2020-04-02 10:55:31 +0530

    Added Retry script and removed unwanted old scripts

--- delopy_rsync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#	Author:	@vipink1203
-#	Web:	www.webnuxpro.com
-#	Automated deployment Script using rsync (secure copy - ssh) 	
-#	Last	Edited:	March	23	2017
-
-
-rsync -rzcSLhe "ssh -p2200" Public/ root@host.example.com:/home/vipinkumar/public_html && ssh -p2200 root@host.example.com "chown -R vipinkumar:vipinkumar /home/vipinkumar/public_html/"
---
+++ retry_until.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#	Author:	@vipink1203
+#	Web:	www.vipinkumar.me
+#	Script to run a command in loop with different values \
+#           but if it fails then it should retry the same again in 10 secs
+#	LastEdited:	1 Apr 2020
+
+
+months=(5 6 7 8 9 10 11 12)
+for month in "${months[@]}";
+do
+  total_days=`cal "${month}" 2020 | awk 'NF {DAYS = $NF}; END {print DAYS}'`
+
+  for (( i = 1; i <= ${total_days}; i++));
+  do
+    until php artisan cron:create_email --asof="2020-${month}-${i}"; do
+        echo Server not reachable, retrying in 10 seconds...
+        sleep 10
+    done
+  done
+done
